a10e15b399329c4d15f41240a5eca81d2e10ebab
kosciak1 <kosciak1@766d2e47-73ce-253c-3ee9-7ff6762cbd2f>
2010-08-15 09:30:08 +0000

    since python-xlib is licensed under GPLv2 it seems I have to license PyWO udner GPL too
    git-svn-id: http://pywo.googlecode.com/svn/trunk@21 766d2e47-73ce-253c-3ee9-7ff6762cbd2f

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 #
+# PyWO - Python Windows Organizer
 # Copyright 2010, Wojciech 'KosciaK' Pietrzok
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is part of PyWO.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# PyWO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# PyWO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyWO.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 """config.py encapsulates dealing with configuration file."""
